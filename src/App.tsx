fafefe13f523cd0874bdc3a22852d257782d0747
Michael Oliver <michael@usekojo.com>
2023-05-12 21:03:00 -0700

    Theme and clean layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
+import { Breadcrumb, Card, ConfigProvider, Layout, Menu, theme } from 'antd';
+import { HomeFilled, HomeOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
 import type { FC } from 'react';
 import 'antd/dist/reset.css';
 import './App.css';
 
 import { simulate } from './utils/simulate';
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    key: 'home',
+    label: 'Home',
+    icon: <HomeFilled />,
+  },
+];
 
 const App: FC = () => (
-  <div className="layout">
-    <div className="sidebar">sidebar</div>
-    <div className="main">
-      <div className="header">header</div>
-      <div className="content">content</div>
-      <div className="footer">Made by Code &lt;3</div>
-    </div>
-  </div>
+  <ConfigProvider
+    theme={{
+      algorithm: theme.darkAlgorithm,
+      token: {
+        colorPrimary: '#F4801A',
+      },
+    }}
+  >
+    <Layout>
+      <Layout.Header
+        style={{
+          backgroundColor: 'transparent',
+          borderBottom: '1px solid rgba(255, 255, 255, 0.1)',
+          height: '48px',
+          lineHeight: '48px',
+        }}
+      >
+        <h1 style={{ fontSize: '16px', fontWeight: 400 }}>EPGP Simulator</h1>
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider style={{ backgroundColor: 'transparent' }}>
+          <Menu items={items} style={{ backgroundColor: 'transparent', borderRight: 0 }} />
+        </Layout.Sider>
+        <Layout.Content style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb
+            items={[{ title: <HomeOutlined /> }, { title: 'Simulator' }]}
+            style={{ padding: '12px' }}
+          />
+          <Card title="Sim time!"></Card>
+        </Layout.Content>
+      </Layout>
+      <Layout.Footer
+        style={{ borderTop: '1px solid rgba(255, 255, 255, 0.1)', textAlign: 'center' }}
+      >
+        Made by Code &lt;3
+      </Layout.Footer>
+    </Layout>
+  </ConfigProvider>
 );
 
 // Add player button (with name, + form for simulation params)

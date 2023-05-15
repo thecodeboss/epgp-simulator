0bc085c8d2be6e13ebc1fb29820864bae0043963
Michael Oliver <michael@usekojo.com>
2023-05-15 15:45:52 -0700

    Add footer to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,11 +6,17 @@ export const Sidebar: FC = () => {
       style={{
         backgroundColor: 'rgba(255, 255, 255, 0.04)',
         borderRight: '1px solid rgba(255, 255, 255, 0.04)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
         padding: '1rem',
         width: '400px',
       }}
     >
-      <h2>Config</h2>
+      <div>
+        <h2>Config</h2>
+      </div>
+      <div>Made by Code &lt;3</div>
     </div>
   );
 };

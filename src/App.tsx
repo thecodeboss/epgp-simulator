912316db074fe73b856e69bf3c9382021a999b8b
Michael Oliver <michael@usekojo.com>
2023-05-16 10:31:44 -0700

    Metadata changes

--- src/App.tsx
+++ src/App.tsx
@@ -86,15 +86,4 @@ const App: FC = () => {
   );
 };
 
-// Add player button (with name, + form for simulation params)
-// - Simulate them winning an item on a given day
-// Add a chart showing each player's EP/GP over time
-// Edit player button
-// Hide player button
-// Delete player button
-// TODO: how to handle attendance?
-// Save config profile button
-// - are you sure you want to overwrite?
-// Load config profile button
-
 export default App;
